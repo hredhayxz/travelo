cb2859b130f8ea7bdd645550fbe3e7d7ce7a81fe
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-11-26 20:12:45 +0600

    Splash Screen design has been done

--- lib/application/app.dart
+++ lib/application/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:travelo/application/state_holder_binder.dart';
 import 'package:travelo/presentation/ui/screens/splash_screen.dart';
 
 class Travelo extends StatelessWidget {
@@ -15,8 +14,7 @@ class Travelo extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      initialBinding: StateHolderBinder(),
-      home: SplashScreen(),
+      home: const SplashScreen(),
     );
   }
 }
