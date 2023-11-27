b321c4cb21613da1bccb22732c5ec1bb67a01882
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-11-27 12:12:30 +0600

    Network caller has been added

---
+++ lib/data/services/network_caller.dart
@@ -0,0 +1,41 @@
+import 'dart:developer';
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:travelo/data/models/network_response.dart';
+import 'package:travelo/presentation/state_holders/auth/auth_controller.dart';
+
+class NetworkCaller {
+
+  /// post request method
+  Future<NetworkResponse> postRequest(String url, Map<String, dynamic> body,
+      {bool isLogin = false}) async {
+    try {
+      Response response = await post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+          'token': AuthController.accessToken.toString()
+        },
+        body: jsonEncode(body),
+      );
+      log(response.statusCode.toString());
+      log(response.body);
+      if (response.statusCode == 200) {
+        return NetworkResponse(
+          true,
+          response.statusCode,
+          jsonDecode(response.body),
+        );
+      } else if (response.statusCode == 401) {
+        if (isLogin == false) {
+          //gotoLogin();
+        }
+      } else {
+        return NetworkResponse(false, response.statusCode, null);
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return NetworkResponse(false, -1, null);
+  }
+}
